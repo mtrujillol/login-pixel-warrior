17158f5b1a8f10a192a80e2b9f66c6777a6a6c79
Marco Trujillo <mtrujillola@autonoma.edu.pe>
2025-05-17 10:50:57 -0500

    Se agrega el proyecto

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "./gesture-handler";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
+
+export default function App() {
+  const Stack = createStackNavigator();
+
+  function MyStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ screens/Login.js
@@ -0,0 +1,12 @@
+import { Text, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+
+export default function Login() {
+  return (
+    <View>
+      <Text>Login</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
