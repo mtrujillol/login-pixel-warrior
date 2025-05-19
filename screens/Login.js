363d993c837ecb5bf43559d7743f21ce2c06c89a
Marco Trujillo <mtrujillola@autonoma.edu.pe>
2025-05-19 16:32:23 -0500

    Se agrega cambios 19/05

--- screens/Login.js
+++ screens/Login.js
@@ -1,4 +1,11 @@
-import { Text, StyleSheet, View, Image } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
 
 export default function Login() {
@@ -10,7 +17,22 @@ export default function Login() {
           style={styles.profile}
         />
       </View>
-      <View styke={styles.targeta}></View>
+      <View styke={styles.targeta}>
+        <View styke={styles.cajaTexto}>
+          <TextInput
+            placeholder="correo@gmail.com"
+            style={{ paddingHorizontal: 15 }}
+          />
+        </View>
+        <View styke={styles.cajaTexto}>
+          <TextInput placeholder="Password" style={{ paddingHorizontal: 15 }} />
+        </View>
+        <View style={styles.PadreBoton}>
+          <TouchableOpacity style={styles.cajaBoton}>
+            <Text style={styles.TextoBoton}>Iniciar Sesion</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
@@ -34,7 +56,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     width: "90%",
     padding: 20,
-    shadowColor: "#000",
+    shadowColor: "#000000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -42,5 +64,26 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
+  },
+  cajaTexto: {
+    paddingVertical: 20,
+    backgroundColor: "#ccccc40",
+    borderRadius: 30,
+    marginVertical: 10,
+  },
+  PadreBoton: {
+    palignItems: "center",
+  },
+
+  cajaBoton: {
+    backgroundColor: "#525FE1",
+    borderRadius: 30,
+    paddingVertical: 15,
+    width: 160,
+    marginTop: 20,
+  },
+  TextoBoton: {
+    textAlign: "center",
+    color: "white",
   },
 });
