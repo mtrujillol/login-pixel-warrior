0bb4e13ce2acf7dbf60bb5fb0b3e35a01f97a7bc
Marco Trujillo <mtrujillola@autonoma.edu.pe>
2025-05-19 16:05:26 -0500

    Se agrega cambios 19/05

--- screens/Login.js
+++ screens/Login.js
@@ -10,6 +10,7 @@ export default function Login() {
           style={styles.profile}
         />
       </View>
+      <View styke={styles.targeta}></View>
     </View>
   );
 }
@@ -26,5 +27,20 @@ const styles = StyleSheet.create({
     height: 100,
     borderRadius: 50,
     borderColor: "white",
+  },
+  targeta: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    width: "90%",
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
